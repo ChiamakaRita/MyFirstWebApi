943266b8b07fa512047a94f0699b6bcef5c259f6
ChiamakaRita <rchiamaka331@gmail.com>
2022-10-22 17:10:11 +0100

    End of section 2

---
+++ MyFirstWeb/MyfirstWeb.Api/Data/MWebDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyfirstWeb.Api.Models.Domain;
+
+namespace MyfirstWeb.Api.Data
+{
+    public class MWebDBContext: DbContext
+    {
+        public MWebDBContext(DbContextOptions<MWebDBContext> options): base(options)
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
