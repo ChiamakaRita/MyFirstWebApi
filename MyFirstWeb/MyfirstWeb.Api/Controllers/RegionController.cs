1b24256921a58698be070e3253fc86560bf02137
ChiamakaRita <rchiamaka331@gmail.com>
2022-10-23 22:50:02 +0100

    End of section 4

--- MyFirstWeb/MyfirstWeb.Api/Controllers/RegionController.cs
+++ MyFirstWeb/MyfirstWeb.Api/Controllers/RegionController.cs
@@ -1,7 +1,10 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using MyfirstWeb.Api.Models.Domain;
+using MyfirstWeb.Api.Models.DTO;
 using MyfirstWeb.Api.Repositories;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
 
 namespace MyfirstWeb.Api.Controllers
 {
@@ -20,7 +23,7 @@ namespace MyfirstWeb.Api.Controllers
 
 
         [HttpGet]
-        public async Task<IActionResult> GetAllRegion()
+        public async Task<IActionResult> GetAllRegionAsync()
         {
             var regions = await regionRepository.GetAllAsync();
             // RETURNS DTO REGION
@@ -36,12 +39,117 @@ namespace MyfirstWeb.Api.Controllers
             //        Lat = region.Lat,
             //        Long = region.Long,
             //        Population = region.Population,
-                    
+
             //    };
             //    regionsDTO.Add(regionDTO);
             //});
             var regionsDTO = mapper.Map<List<Models.DTO.Region>>(regions);
             return Ok(regionsDTO);
+        }
+        [HttpGet]
+        [Route("{id:Guid}")]
+        [ActionName("GetRegionAsync")]
+        public async Task<IActionResult> GetRegionAsync(Guid id)
+        {
+            var region = await regionRepository.GetAsync(id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+            var regionDTO = mapper.Map<Models.DTO.Region>(region);
+            return Ok(regionDTO);
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> AddRegionAsync(AddRegionRequest addRegionRequest)
+        {
+            //REQUEST(DTO) TO DOMAIN MODEL
+            var region = new Models.Domain.Region()
+            {
+                Code = addRegionRequest.Code,
+                Name = addRegionRequest.Name,
+                Area = addRegionRequest.Area,
+                Lat = addRegionRequest.Lat,
+                Long = addRegionRequest.Long,
+                Population = addRegionRequest.Population,
+            };
+            //pass details to repository
+            region = await regionRepository.AddAsync(region);
+
+            //convert back to DTO
+            var regionDTO = new Models.DTO.Region()
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                Area = region.Area,
+                Lat = region.Lat,
+                Long = region.Long,
+                Population = region.Population,
+            };
+            return CreatedAtAction(nameof(GetRegionAsync), new {id = regionDTO.Id}, regionDTO);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> DeleteRegionAsync(Guid id)
+        {
+            //get a region from the database
+            var region = await regionRepository.DeleteAsync(id);
+            //if not found
+            if (region == null)
+            {
+                return NotFound();
+            }
+            //convert response back to DTO
+            var regionDTO = new Models.DTO.Region
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                Area = region.Area,
+                Lat = region.Lat,
+                Long = region.Long,
+                Population = region.Population,
+            };
+            //return ok response
+            return Ok(regionDTO);
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> UpdateRegionAsync([FromRoute]Guid id, [FromBody] UpdateRegionRequest updateRegionRequest)
+        {
+            //convert DTO to Model domain
+            var region = new Models.Domain.Region()
+            {
+                Code = updateRegionRequest.Code,
+                Name = updateRegionRequest.Name,
+                Area = updateRegionRequest.Area,
+                Lat = updateRegionRequest.Lat,
+                Long = updateRegionRequest.Long,
+                Population = updateRegionRequest.Population,
+            };
+            //update region using repository
+            region = await regionRepository.UpdateAsync(id, region);
+            //if null then notfound
+            if (region == null)
+            {
+                return NotFound();
+            }
+            //convert domain back to DTO
+            var regionDTO = new Models.DTO.Region()
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                Area = region.Area,
+                Lat = region.Lat,
+                Long = region.Long,
+                Population = region.Population,
+            };
+            //return ok response
+            return Ok(regionDTO);
         }
     }
 }
--- MyFirstWeb/MyfirstWeb.Api/Repositories/RegionRepository.cs
+++ MyFirstWeb/MyfirstWeb.Api/Repositories/RegionRepository.cs
@@ -13,9 +13,57 @@ namespace MyfirstWeb.Api.Repositories
             this.mWebDBContext = mWebDBContext;
         }
 
+        public async Task<Region> AddAsync(Region region)
+        {
+            region.Id = Guid.NewGuid();
+            await mWebDBContext.AddAsync(region);
+            await mWebDBContext.SaveChangesAsync();
+            return region;
+        }
+
+        public async Task<Region> DeleteAsync(Guid id)
+        {
+            var region = await mWebDBContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+            if (region == null)
+            {
+                return null;
+            }
+            //delete region
+            mWebDBContext.Regions.Remove(region);
+            await mWebDBContext.SaveChangesAsync();
+            return region;
+        }
+
         public async Task<IEnumerable<Region>> GetAllAsync()
         {
-           return await mWebDBContext.Regions.ToListAsync();
+            return await mWebDBContext.Regions.ToListAsync();
         }
+
+        public async Task<Region> GetAsync(Guid id)
+        {
+            return await mWebDBContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Region> UpdateAsync(Guid id, Region region)
+        {
+            var existingRegion = await mWebDBContext.Regions.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(existingRegion == null)
+            {
+                return null;
+            }
+            existingRegion.Code = region.Code;
+            existingRegion.Name = region.Name;
+            existingRegion.Area = region.Area;
+            existingRegion.Lat = region.Lat;
+            existingRegion.Long = region.Long;
+            existingRegion.Population = region.Population;
+
+            await mWebDBContext.SaveChangesAsync();
+
+            return existingRegion;
+        }
+
     }
 }
+
